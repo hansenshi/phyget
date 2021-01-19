fe3c7a4fec6933f89e8dc331ee10748bb5b9f82c
石宁宁 <hansen.shi@hotmail.com>
2021-01-19 18:35:04 +0800

    Delete backup.sh

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-curtime=`date +%Y%m%d_%H%M%S`
-
-for i in phyget_{head,inspur,dell,hp}.sh; do
-  cp $i backup/${i}_${curtime} && echo -e " Backup ${i} complete" | awk '{printf " %-8s%-18s%-10s\n",$1,$2,$3}'
-done
